ebb18545668895af8126c419dc3178c37c78a45c
Nastya Sukhoparova <nastyadalchenko1997@gmail.com>
2024-10-24 13:18:47 +0300

    friend

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Profile from "./Profile/Profile";
+import userData from "../userData.json";
+import FriendList from "./FriendList/FriendListItem";
+import friends from "../friends.json";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./FriendListItem.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div className={styles.friendItem}>
+      <span className={isOnline ? styles.online : styles.offline}></span>
+      <img
+        src={avatar}
+        alt={`${name}'s avatar`}
+        width="48"
+        className={styles.avatar}
+      />
+      <p className={styles.name}>{name}</p>
+    </div>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+export default FriendListItem;
